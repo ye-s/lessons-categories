9f1ee4f231f40b5600098de99fcf4087d09d7927
Yevgeniy <jenyaslu@gmail.com>
2019-04-26 00:39:54 +0300

    Updates

---
+++ precache-manifest.555a4d3300fdbf0274e5d5741c9110c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8f4a6102b1a0981a2b3",
+    "url": "/lessons-categories/static/js/runtime~main.a7664e2e.js"
+  },
+  {
+    "revision": "f1f568feda9049b0f30b",
+    "url": "/lessons-categories/static/js/main.1d0549c2.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/lessons-categories/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f1f568feda9049b0f30b",
+    "url": "/lessons-categories/static/css/main.f17f3c6a.chunk.css"
+  },
+  {
+    "revision": "abe3ba53516d504373f1d398ba09d72d",
+    "url": "/lessons-categories/index.html"
+  }
+];
